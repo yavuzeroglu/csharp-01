7f5a779012c821530090b6e53c8b351953140c3e
Yavuz Eroglu <yavuzeroglu15@gmail.com>
2022-06-22 22:38:39 +0300

    program.cs eklendi

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string inputString;
+            inputString = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(inputString);
+
+            Console.ReadKey();
+
+        }
+    }
+}
